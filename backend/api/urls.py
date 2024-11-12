40f58d621b2fa8e28b9986ca5ebb65f955b13a3d
ViceSlad <vicevod@gmail.com>
2024-11-12 01:11:08 +0100

    Add mjenjanje lozinke

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     #log in i log out implementirani preko djanga
+
+    path('accountManager/', views.account_page, name='account_page'),
+    path('accountManager/password_change/', views.change_password, name='password_change')
 ]
