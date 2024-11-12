acb512cdbda4e9ca7b74955ff019c0bad304910a
PetarKrtalic <petar.krtalic@fer.unizg.hr>
2024-11-12 01:33:37 +0100

    Added account information page design
    Added design for account information page, removed some files that came default when building REACT project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Login from './Components/Login/Login';
+import AccountInfo from './Components/AccountInfo/AccountInfo';
 
 function App() {
   return (
     <div>
-      <Login/>
+      <AccountInfo/>
     </div>
   );
 }
